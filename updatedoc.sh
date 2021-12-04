da6f98b99a9c34a50102b670da4727d21f10edcc
Georgii Perepecho <horprogs@gmail.com>
2021-12-04 20:22:42 +0000

    ci(): play with semantic release

--- updatedoc.sh
+++ updatedoc.sh
@@ -1,1 +1,1 @@
-sed -i '' "s/just-validate@$1/just-validate@$2/g" README.md  site/index.html site/examples/index.html
+sed -i -e "s/just-validate@$1/just-validate@$2/g" README.md  site/index.html site/examples/index.html
