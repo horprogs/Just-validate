c62766697b0e2df972cb59c83a2d6844ab5c3e10
Georgii Perepecho <horprogs@gmail.com>
2022-03-17 19:15:21 +0000

    docs(): add documentation website section

--- site/vite.config.js
+++ site/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ command }) => {
           input: {
             main: path.resolve(__dirname, 'index.html'),
             examples: path.resolve(__dirname, 'examples/index.html'),
+            documentation: path.resolve(__dirname, 'documentation/index.html'),
           },
         },
       },
